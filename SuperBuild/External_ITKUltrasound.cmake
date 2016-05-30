c15421000af0602249c80b83ec9579caa804c416
Matt McCormick <matt.mccormick@kitware.com>
2016-05-29 22:50:27 -0400

    ENH: Bump ITKUltrasound to 2016-05-29

--- SuperBuild/External_ITKUltrasound.cmake
+++ SuperBuild/External_ITKUltrasound.cmake
@@ -7,8 +7,8 @@ set(proj ITKUltrasound)
 set(${proj}_DEPENDENCIES )
 ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
 
-# master 2016-05-25
-set(${proj}_GIT_TAG 972c7be4bdfacb0a41972ef86b99de368da2e1b5)
+# master 2016-05-29
+set(${proj}_GIT_TAG 8f848928f1f794cb46e515f8969902a5b65b30c3)
 ExternalProject_Add(${proj}
   ${${proj}_EP_ARGS}
   GIT_REPOSITORY ${git_protocol}://github.com/KitwareMedical/ITKUltrasound.git
