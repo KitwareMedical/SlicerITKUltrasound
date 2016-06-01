aea42ee3ba928a8d03f05040f47165b369046014
Matt McCormick <matt.mccormick@kitware.com>
2016-06-01 01:50:21 -0400

    BUG: Bump ITKUltrasound for SliceSeries angle direction fix

--- SuperBuild/External_ITKUltrasound.cmake
+++ SuperBuild/External_ITKUltrasound.cmake
@@ -7,8 +7,8 @@ set(proj ITKUltrasound)
 set(${proj}_DEPENDENCIES )
 ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
 
-# disable-wrapping 2016-05-31
-set(${proj}_GIT_TAG 0c4d6594ef31ac1f36278f97081b578532474547)
+# disable-wrapping 2016-06-01
+set(${proj}_GIT_TAG 3468f98076b617ea2d1704a78db4a0f6e6b8cedf)
 ExternalProject_Add(${proj}
   ${${proj}_EP_ARGS}
   GIT_REPOSITORY ${git_protocol}://github.com/thewtex/ITKUltrasound.git
