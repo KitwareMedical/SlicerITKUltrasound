e1a9ddf2eb28ad16159c4d19909ae18d90291c90
Dženan Zukić <dzenan.zukic@kitware.com>
2023-08-01 13:46:06 -0400

    Remove compiled modules from SuperBuild

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -22,7 +22,6 @@ if(NOT CMAKE_CONFIGURATION_TYPES)
 endif()
 
 set(proj ${SUPERBUILD_TOPLEVEL_PROJECT})
-set(${proj}_DEPENDS ITKStrain ITKSplitComponents ITKHigherOrderAccurateGradient ITKBSplineGradient ITKUltrasound)
 
 ExternalProject_Include_Dependencies(${proj}
   PROJECT_VAR proj
